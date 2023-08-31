14934e9037d7f11c2c092dbd4a5d432fca0e81d9
Marco Barría <marco.barria.palma@gmail.com>
2023-08-31 11:42:44 -0400

    MarcoAurelio

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
-seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
+const prompt = require("prompt-sync")(); 
+/*seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
 if (seguir == "s") {
     opcion = prompt("1.- suma, 2.- resta 3.- Multiplicacion 4.- Division, que quiere hacer? ");
     if (opcion == 1) {
@@ -28,8 +29,66 @@ if (seguir == "s") {
     n2 = parseFloat(prompt("Ingrese número 2: "));
     resultado = n1 / n2;
     console.log(resultado)
+}*/
+function suma(a, b) {
+    return a + b;
 }
 
+function resta(a, b) {
+    return a - b;
+}
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        throw new Error("No se puede dividir entre cero");
+    }
+}
+
+while (true) {
+    console.log("Operaciones disponibles:");
+    console.log("1. Suma");
+    console.log("2. Resta");
+    console.log("3. Multiplicación");
+    console.log("4. División");
+    console.log("5. Salir");
+
+    const opcion = prompt("Selecciona una operación (1/2/3/4/5):");
+
+    if (opcion === '5') {
+        console.log("¡Hasta luego!");
+        break;
+    }
+
+    try {
+        const num1 = parseFloat(prompt("Ingresa el primer número:"));
+        const num2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+        switch (opcion) {
+            case '1':
+                console.log("Resultado:", suma(num1, num2));
+                break;
+            case '2':
+                console.log("Resultado:", resta(num1, num2));
+                break;
+            case '3':
+                console.log("Resultado:", multiplicacion(num1, num2));
+                break;
+            case '4':
+                console.log("Resultado:", division(num1, num2));
+                break;
+            default:
+                console.log("Opción inválida. Por favor, selecciona una opción válida.");
+        }
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
 
 
 
